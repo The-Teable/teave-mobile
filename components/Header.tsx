80d1a0ed5f0a1069a021a85c9e696a804e225dce
ks <ogs0518@gmail.com>
2022-06-21 01:29:34 +0900

    design: 탭바에 아이콘 이름추가, rem으로 사이즈 변경

--- components/Header.tsx
+++ components/Header.tsx
@@ -7,13 +7,13 @@ const Container = styled.div`
   align-items: center;
   max-width: 76.8rem;
   margin: 0 auto;
+  padding: 0 1.3rem;
 `;
 
 const Logo = styled.div`
   background: url("/image/logo_text.png") no-repeat center/contain;
-  width: 160px;
-  height: 32px;
-  margin: 14px;
+  width: 7.6rem;
+  height: 4.8rem;
   &:hover {
     cursor: pointer;
   }
@@ -21,9 +21,8 @@ const Logo = styled.div`
 
 const Menu = styled.div`
   background: url("/image/icon_menu.svg") no-repeat center/contain;
-  width: 32px;
-  height: 32px;
-  margin: 14px;
+  width: 3.2rem;
+  height: 3.2rem;
   &:hover {
     cursor: pointer;
   }
--- components/TabBar.tsx
+++ components/TabBar.tsx
@@ -1,8 +1,11 @@
 import styled from "styled-components";
 import Link from "next/link";
 
-interface itemProps {
+interface iconProps {
   url: string;
+}
+
+interface textProps {
   text: string;
 }
 
@@ -12,7 +15,7 @@ const Container = styled.div`
   align-items: center;
   max-width: 76.8rem;
   margin: 0 auto;
-  height: 8rem;
+  height: 4.8rem;
   position: fixed;
   right: 0;
   bottom: 0;
@@ -20,27 +23,40 @@ const Container = styled.div`
   border-top: 0.1rem solid #e6e6e6;
 `;
 
-const Item = styled.div<itemProps>`
-  background: url(${({ url }) => url}) no-repeat center/contain;
+const Item = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
   width: 100%;
-  height: 32px;
-  margin: 5px;
+  height: 100%;
   &:hover {
     cursor: pointer;
   }
 `;
 
+const Icon = styled.div<iconProps>`
+  background: url(${({ url }) => url}) no-repeat center/contain;
+  width: 2.5rem;
+  height: 2.5rem;
+`;
+
+const Text = styled.span``;
+
 const items = [
   { url: "image/icon_home.svg", text: "홈", href: "/" },
   { url: "image/icon_search.svg", text: "검색", href: "/" },
   { url: "image/icon_cart.svg", text: "장바구니", href: "/" },
-  { url: "image/icon_account.svg", text: "마이페이지", href: "/" }
+  { url: "image/icon_account.svg", text: "마이페이지", href: "/" },
 ];
 const TabBar = () => (
   <Container>
     {items.map(({ url, text, href }, i) => (
       <Link key={i} href={href} passHref>
-        <Item url={url} text={text} />
+        <Item>
+          <Icon url={url} />
+          <Text>{text}</Text>
+        </Item>
       </Link>
     ))}
   </Container>
