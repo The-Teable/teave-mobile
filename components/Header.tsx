ad4325ed2121c0aeb260cc35c5aecc0082e3466e
ks <ogs0518@gmail.com>
2022-06-27 02:58:01 +0900

    feat: 햄버거메뉴 제거, 상단nav 구현

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,51 +3,107 @@ import Link from "next/link";
 import CenteredContainer from "./common/CenteredContainer";
 
 const Container = styled(CenteredContainer).attrs({ as: "header" })`
-  display: flex;
-  justify-content: space-between;
   position: fixed;
   right: 0;
   top: 0;
   left: 0;
   z-index: 1;
+  height: 9rem;
+`;
+
+const TopWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
   height: 5rem;
 `;
 
+const NavWrapper = styled.div`
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  height: 4rem;
+  font-size: 1.4rem;
+`;
 const Logo = styled.div`
   background: url("/image/logo_text.png") no-repeat center/contain;
   width: 6.3rem;
   height: 1.7rem;
+  padding: 0 0.5rem;
   &:hover {
     cursor: pointer;
   }
 `;
 
-const Menu = styled.div`
-  background: url("/image/icon_menu.svg") no-repeat center/contain;
-  width: 3.2rem;
-  height: 3.2rem;
-  margin-right: 2rem;
-  &:hover {
-    cursor: pointer;
-  }
+const LoginLogOutButton = styled.button`
+  background-color: #ffffff;
+  border: 0;
+  color: #424242;
+  height: 2rem;
+  font-size: 1rem;
 `;
+
+const NavItemContainer = styled.div`
+  display: flex;
+  align-items: center;
+  height: 85%;
+`;
+
+const Selected = styled(NavItemContainer)`
+  border-bottom: solid #1a4c45 0.3rem;
+`;
+
+const NavItemTitle = styled.span``;
 
 const Padding = styled.div`
-  height: 5rem;
+  height: 9rem;
 `;
 
-const Header = () => (
-  <>
-    <Container>
-      <Link href="/" passHref>
-        <Logo />
-      </Link>
-      <Link href="/menu" passHref>
-        <Menu />
-      </Link>
-    </Container>
-    <Padding />
-  </>
-);
+const items = [
+  { title: "메인", href: "/", selected: true },
+  { title: "샵", href: "/", selected: false },
+  { title: "테스트", href: "/", selected: false },
+  { title: "매거진", href: "/", selected: false },
+];
+
+const Header = () => {
+  const isLogin = false;
+  return (
+    <>
+      <Container>
+        <TopWrapper>
+          <Link href="/" passHref>
+            <Logo />
+          </Link>
+          {!isLogin ? (
+            <Link href="/" passHref>
+              <LoginLogOutButton>로그인 / 회원가입</LoginLogOutButton>
+            </Link>
+          ) : (
+            <Link href="/" passHref>
+              <LoginLogOutButton>로그아웃</LoginLogOutButton>
+            </Link>
+          )}
+        </TopWrapper>
+        <NavWrapper>
+          {items.map(({ title, href, selected }, i) => (
+            <Link key={i} href={href} passHref>
+              {selected ? (
+                <Selected>
+                  <NavItemTitle>{title}</NavItemTitle>
+                </Selected>
+              ) : (
+                <NavItemContainer>
+                  <NavItemTitle>{title}</NavItemTitle>
+                </NavItemContainer>
+              )}
+            </Link>
+          ))}
+        </NavWrapper>
+      </Container>
+      <Padding />
+    </>
+  );
+};
 
 export default Header;
