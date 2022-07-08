190ce5cfc0b74f3effd0267b356395d0ae084359
ggsno <ogs0518@gmail.com>
2022-07-08 15:26:15 +0900

    chores: hooks 복원

---
+++ hooks/useThemeRecommend.tsx
@@ -0,0 +1,21 @@
+import axios from "axios";
+
+const useThemeRecommend = () => {
+  const userId = 123; // temp
+  const instance = axios.create({ baseURL: process.env.NEXT_PUBLIC_LS_URL });
+
+  const onClickProduct = (teaId: any) => {
+    instance.post("user-click-product", {
+      userId,
+      teaId
+    });
+  };
+
+  const onClickFavorite = (teaId: any) => {
+    console.log(teaId, userId); // temp
+  };
+
+  return [onClickProduct, onClickFavorite];
+};
+
+export default useThemeRecommend;
