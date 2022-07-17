2a3ee9a157cd555b04212e6d36b3dfc247f3dc90
ks <ogs0518@gmail.com>
2022-07-17 13:06:27 +0900

    design: 회원가입헤더 구분선

--- pages/mypage/signup.tsx
+++ pages/mypage/signup.tsx
@@ -45,105 +45,103 @@ const SignupPage = () => {
   }, [userid]);
   return (
     <>
-      <CenteredContainer>
-        <Margin size={4} />
-        <S.Container>
-          <S.Header>
-            <Link href={"/mypage/login"} passHref>
-              <S.GoBackButton />
-            </Link>
-            <S.Title>회원가입</S.Title>
-          </S.Header>
-          <Margin size={1.5} />
-          <form onSubmit={handleSubmit}>
-            <S.Label htmlFor="userid">
-              아이디<span style={{ color: "red" }}>*</span>
-            </S.Label>
-            <S.IdContainer>
-              <S.InputId id={"userid"} placeholder={"아이디"} required />
-              <Margin size={1} row />
-              <S.ButtonIdCheck
-                onClick={handleDuplicateIdCheck}
-                reverse={!duplicateIdCheck}
-              >
-                중복확인
-              </S.ButtonIdCheck>
-            </S.IdContainer>
-            <S.Label htmlFor="password">
-              비밀번호<span style={{ color: "red" }}>*</span>
-            </S.Label>
-            <S.InputText
-              id={"password"}
-              placeholder={"비밀번호를 입력해주세요"}
-              type={"password"}
-              required
+      <Margin size={4} />
+      <S.Container>
+        <S.Header>
+          <Link href={"/mypage/login"} passHref>
+            <S.GoBackButton />
+          </Link>
+          <S.Title>회원가입</S.Title>
+        </S.Header>
+        <Margin size={2} />
+        <form onSubmit={handleSubmit}>
+          <S.Label htmlFor="userid">
+            아이디<span style={{ color: "red" }}>*</span>
+          </S.Label>
+          <S.IdContainer>
+            <S.InputId id={"userid"} placeholder={"아이디"} required />
+            <Margin size={1} row />
+            <S.ButtonIdCheck
+              onClick={handleDuplicateIdCheck}
+              reverse={!duplicateIdCheck}
+            >
+              중복확인
+            </S.ButtonIdCheck>
+          </S.IdContainer>
+          <S.Label htmlFor="password">
+            비밀번호<span style={{ color: "red" }}>*</span>
+          </S.Label>
+          <S.InputText
+            id={"password"}
+            placeholder={"비밀번호를 입력해주세요"}
+            type={"password"}
+            required
+          />
+          <S.Label htmlFor="passwordCheck">
+            비밀번호 확인<span style={{ color: "red" }}>*</span>
+          </S.Label>
+          <S.InputText
+            id={"passwordCheck"}
+            placeholder={"비밀번호를 입력해주세요"}
+            type={"password"}
+            required
+          />
+          <S.Label htmlFor="username">이름</S.Label>
+          <S.InputText id={"username"} placeholder={"이름을 입력해주세요"} />
+          <S.Label htmlFor="email">이메일</S.Label>
+          <S.InputText id={"email"} placeholder={"이메일을 입력해주세요"} />
+          <S.Label htmlFor="phone">휴대폰</S.Label>
+          <S.InputText
+            id={"phone"}
+            placeholder={"숫자만 입력해주세요"}
+            type={"tel"}
+          />
+          <S.Label htmlFor="address">주소</S.Label>
+          <S.InputText
+            id={"address"}
+            placeholder={"도로명, 지번, 건물명 검색"}
+            onClick={handleSearchAddress}
+            value={address}
+          />
+          {openPostcode ? (
+            <DaumPostcode
+              onComplete={handleSelectAddress}
+              autoClose={false}
+              defaultQuery={"논현로 66길"}
             />
-            <S.Label htmlFor="passwordCheck">
-              비밀번호 확인<span style={{ color: "red" }}>*</span>
-            </S.Label>
-            <S.InputText
-              id={"passwordCheck"}
-              placeholder={"비밀번호를 입력해주세요"}
-              type={"password"}
-              required
-            />
-            <S.Label htmlFor="username">이름</S.Label>
-            <S.InputText id={"username"} placeholder={"이름을 입력해주세요"} />
-            <S.Label htmlFor="email">이메일</S.Label>
-            <S.InputText id={"email"} placeholder={"이메일을 입력해주세요"} />
-            <S.Label htmlFor="phone">휴대폰</S.Label>
-            <S.InputText
-              id={"phone"}
-              placeholder={"숫자만 입력해주세요"}
-              type={"tel"}
-            />
-            <S.Label htmlFor="address">주소</S.Label>
-            <S.InputText
-              id={"address"}
-              placeholder={"도로명, 지번, 건물명 검색"}
-              onClick={handleSearchAddress}
-              value={address}
-            />
-            {openPostcode ? (
-              <DaumPostcode
-                onComplete={handleSelectAddress}
-                autoClose={false}
-                defaultQuery={"논현로 66길"}
+          ) : null}
+          <S.Label htmlFor="birthday">생년월일</S.Label>
+          <S.InputText
+            id={"birthday"}
+            placeholder={"YYYY / MM / DD"}
+            type="date"
+          />
+          <fieldset>
+            <legend>성별</legend>
+            <S.GenderRadioContainer>
+              <input name="gender" value="male" id="male" type="radio" />
+              <S.GenderRadioOption htmlFor="male">남</S.GenderRadioOption>
+            </S.GenderRadioContainer>
+            <S.GenderRadioContainer>
+              <input name="gender" value="female" id="female" type="radio" />
+              <S.GenderRadioOption htmlFor="female">여</S.GenderRadioOption>
+            </S.GenderRadioContainer>
+            <S.GenderRadioContainer>
+              <input
+                name="gender"
+                value="noselect"
+                id="noselect"
+                type="radio"
               />
-            ) : null}
-            <S.Label htmlFor="birthday">생년월일</S.Label>
-            <S.InputText
-              id={"birthday"}
-              placeholder={"YYYY / MM / DD"}
-              type="date"
-            />
-            <fieldset>
-              <legend>성별</legend>
-              <S.GenderRadioContainer>
-                <input name="gender" value="male" id="male" type="radio" />
-                <S.GenderRadioOption htmlFor="male">남</S.GenderRadioOption>
-              </S.GenderRadioContainer>
-              <S.GenderRadioContainer>
-                <input name="gender" value="female" id="female" type="radio" />
-                <S.GenderRadioOption htmlFor="female">여</S.GenderRadioOption>
-              </S.GenderRadioContainer>
-              <S.GenderRadioContainer>
-                <input
-                  name="gender"
-                  value="noselect"
-                  id="noselect"
-                  type="radio"
-                />
-                <S.GenderRadioOption htmlFor="noselect">
-                  선택안함
-                </S.GenderRadioOption>
-              </S.GenderRadioContainer>
-            </fieldset>
-            <Margin size={2} />
-            <S.Button>제출하기</S.Button>
-          </form>
-        </S.Container>
-      </CenteredContainer>
+              <S.GenderRadioOption htmlFor="noselect">
+                선택안함
+              </S.GenderRadioOption>
+            </S.GenderRadioContainer>
+          </fieldset>
+          <Margin size={2} />
+          <S.Button>제출하기</S.Button>
+        </form>
+      </S.Container>
     </>
   );
 };
@@ -152,7 +150,10 @@ export default SignupPage;
 
 const S: any = {};
 
+S.CenteredContainer = styled(CenteredContainer)``;
+
 S.Container = styled.div`
+  position: relative;
   display: flex;
   flex-direction: column;
   max-width: 40rem;
@@ -162,12 +163,15 @@ S.Container = styled.div`
 `;
 S.Header = styled.div`
   display: flex;
-  height: 2.5rem;
-  padding: 0 0 1.5rem;
+  height: 5rem;
+  align-items: center;
   border-bottom: 1px solid #eeeeee;
+  width: 100%;
 `;
 
 S.GoBackButton = styled.button`
+  position: absolute;
+  left: 2rem;
   border: 0px;
   background: url("/image/icon_go_back.svg") no-repeat center/contain;
   width: 2.5rem;
