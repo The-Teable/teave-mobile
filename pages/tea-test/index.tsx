e0a33d8723ad6510c941261c59a06e7820a717ec
ks <ogs0518@gmail.com>
2022-07-19 14:45:46 +0900

    style: 티테스트 메인페이지 마크업

--- pages/tea-test/index.tsx
+++ pages/tea-test/index.tsx
@@ -3,12 +3,24 @@ import styled from "styled-components";
 import Button from "../../components/common/Button";
 import Link from "next/link";
 import { HeaderIndex } from "../../components/layout/Header";
+import Margin from "../../components/common/Margin";
 
 const TestPage = () => {
   return (
     <HomeLayout headerIndex={HeaderIndex.TEST}>
       <S.Container>
-        <S.Text>내 취향에 꼭 맞는 어쩌구저쩌구</S.Text>
+        <S.Text>
+          내 취향에 꼭 맞는 티와 함께
+          <br />
+          여유로운 하루를 보내고 싶지 않으신가요?
+        </S.Text>
+        <Margin size={2} />
+        <S.SubText>
+          간단한 테스트를 통해
+          <br />
+          여러분의 취향에 맞는 티를 찾아드릴게요!
+        </S.SubText>
+        <Margin size={5} />
         <Link href="/tea-test/question" passHref>
           <S.Button>티 테스트 시작</S.Button>
         </Link>
@@ -19,14 +31,27 @@ const TestPage = () => {
 
 const S: any = {};
 
-S.Container = styled.section``;
-
-// temp style
-S.Text = styled.p`
-  font-size: 20px;
-  margin-top: 50px;
+S.Container = styled.section`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  height: 80vh;
 `;
 
-S.Button = styled(Button)``;
+S.Text = styled.p`
+  font-size: 1.6rem;
+  text-align: center;
+`;
+
+S.SubText = styled.p`
+  font-size: 1.2rem;
+  text-align: center;
+  color: #424242;
+`;
+
+S.Button = styled(Button)`
+  width: 25rem;
+`;
 
 export default TestPage;
