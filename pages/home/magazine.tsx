305f7eb028d9b26f8d7f85d15953303e82c99eda
ggsno <ogs0518@gmail.com>
2022-07-26 18:09:45 +0900

    feat: shop init

--- pages/home/magazine.tsx
+++ pages/home/magazine.tsx
@@ -1,12 +1,23 @@
 import HomeLayout from "../../components/layout/HomeLayout";
 import { HOME_HEADER_INDEX } from "../../components/layout/HomeHeader";
+import styled from "styled-components";
 
 const MagazinePage = () => {
   return (
     <HomeLayout headerIndex={HOME_HEADER_INDEX.MAGAZINE}>
-      <div>준비중입니다</div>
+      <S.Container>준비중입니다.</S.Container>
     </HomeLayout>
   );
 };
 
 export default MagazinePage;
+
+const S: any = {};
+
+S.Container = styled.section`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 80vh;
+  font-size: 2rem;
+`;
--- pages/home/shop.tsx
+++ pages/home/shop.tsx
@@ -1,12 +1,26 @@
 import HomeLayout from "../../components/layout/HomeLayout";
 import { HOME_HEADER_INDEX } from "../../components/layout/HomeHeader";
+import styled from "styled-components";
+import CenteredContainer from "../../components/layout/CenteredContainer";
+import { useState } from "react";
+import { color } from "../../styles/palette";
 
 const ShopPage = () => {
+  const [filter, setFilter] = useState([]);
   return (
     <HomeLayout headerIndex={HOME_HEADER_INDEX.SHOP}>
+      {filter.length > 0 ? <S.FilterHeader></S.FilterHeader> : null}
       <div>샵</div>
     </HomeLayout>
   );
 };
 
 export default ShopPage;
+
+const S: any = {};
+
+S.FilterHeader = styled.div``;
+
+S.FilterItem = styled.div`
+  background-color: ${color.teaveGreen};
+`;
